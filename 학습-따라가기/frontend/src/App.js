bdf7a23dbe3e7654ef9da13a3187181b65509f58
JungSik Jeong <wndtlr1024@gmail.com>
2024-09-26 22:25:37 +0900

    학습따라가기 1-1

---
+++ 학습-따라가기/frontend/src/App.js
@@ -0,0 +1,49 @@
+console.log("app is running!");
+
+class App {
+  $target = null; // dom을 가르키는 표시를 $로함
+  data = [];
+
+  constructor($target) {
+    this.$target = $target;
+
+    this.darkModeToggle = new DarkModeToggle({
+      $target,
+      // onSearch: (keyword) => {
+      //   api.fetchCats(keyword).then(({ data }) => this.setState(data));
+      // },
+    });
+
+    this.searchInput = new SearchInput({
+      $target,
+      onSearch: (keyword) => {
+        api.fetchCats(keyword).then(({ data }) => this.setState(data));
+      },
+    });
+
+    this.searchResult = new SearchResult({
+      $target,
+      initialData: this.data,
+      onClick: (image) => {
+        this.imageInfo.setState({
+          visible: true,
+          image,
+        });
+      },
+    });
+
+    this.imageInfo = new ImageInfo({
+      $target,
+      data: {
+        visible: false,
+        image: null,
+      },
+    });
+  }
+
+  setState(nextData) {
+    console.log(this);
+    this.data = nextData;
+    this.searchResult.setState(nextData);
+  }
+}
