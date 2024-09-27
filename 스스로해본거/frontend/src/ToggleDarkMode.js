810aaa607f6b7aa03a52b1d012250722cc63816f
JungSik Jeong <wndtlr1024@gmail.com>
2024-09-27 09:30:12 +0900

    스스로해보기- darkMode,html Tag 변경 등등

---
+++ 스스로해본거/frontend/src/ToggleDarkMode.js
@@ -0,0 +1,35 @@
+class ToggleDarkMode {
+  isDarkMode = null;
+
+  constructor({ $target, onSearch }) {
+    const $wrapper = document.createElement('section');
+    const $toggleDarkMode = document.createElement('input');
+    this.$toggleDarkMode = $toggleDarkMode;
+    this.$toggleDarkMode.type = 'checkbox';
+
+    $toggleDarkMode.className = 'ToggleDarkMode';
+    $wrapper.appendChild($toggleDarkMode);
+    $target.appendChild($wrapper);
+
+    $toggleDarkMode.addEventListener('change', (e) => {
+      this.setColorMode(e.target.checked);
+    });
+
+    this.initColorMode();
+  }
+
+  initColorMode(isDarkMode) {
+    this.isDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches;
+    this.$toggleDarkMode.checked = this.isDarkMode;
+
+    console.log(this.isDarkMode);
+    this.setColorMode(this.isDarkMode);
+  }
+
+  setColorMode(isDarkMode) {
+    document.documentElement.setAttribute(
+      'color-mode',
+      isDarkMode ? 'dark' : 'light'
+    );
+  }
+}
