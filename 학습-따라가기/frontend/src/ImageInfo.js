c287de9cdc8ec3766148f4d59b37345ac68a2e42
jungsikJeong <wndtlr1024@gmail.com>
2024-10-05 16:55:13 +0900

    학습-따라가기/ 모달클릭시 닫힘

--- 학습-따라가기/frontend/src/ImageInfo.js
+++ 학습-따라가기/frontend/src/ImageInfo.js
@@ -3,8 +3,8 @@ class ImageInfo {
   data = null;
 
   constructor({ $target, data }) {
-    const $imageInfo = document.createElement("div");
-    $imageInfo.className = "ImageInfo";
+    const $imageInfo = document.createElement('div');
+    $imageInfo.className = 'ImageInfo';
     this.$imageInfo = $imageInfo;
     $target.appendChild($imageInfo);
 
@@ -18,9 +18,24 @@ class ImageInfo {
     this.render();
   }
 
+  showDetail(data) {
+    // 상세 정보 요청
+    api.fetchCatDetail(data.cat.id).then(({ data }) =>
+      this.setState({
+        visible: true,
+        cat: data,
+      })
+    );
+  }
+
+  closeImageInfo() {
+    console.log('닫기');
+    this.setState({ visible: false, cat: undefined });
+  }
+
   render() {
     if (this.data.visible) {
-      const { name, url, temperament, origin } = this.data.image;
+      const { name, url, temperament, origin } = this.data.cat;
 
       this.$imageInfo.innerHTML = `
         <div class="content-wrapper">
@@ -34,9 +49,30 @@ class ImageInfo {
             <div>태생: ${origin}</div>
           </div>
         </div>`;
-      this.$imageInfo.style.display = "block";
+      this.$imageInfo.style.display = 'block';
+
+      // this.$imageInfo.querySelector('.close').addEventListener('click', (e) => {
+      //   this.closeImageInfo();
+      // });
+
+      // TODO: keypress,keydown,keyup 차이 리서치해보기
+      document.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+          this.closeImageInfo();
+        }
+      });
+
+      this.$imageInfo.addEventListener('click', (e) => {
+        // console.log(e.target.className);
+        if (
+          e.target.className === 'ImageInfo' ||
+          e.target.className === 'close'
+        ) {
+          this.closeImageInfo();
+        }
+      });
     } else {
-      this.$imageInfo.style.display = "none";
+      this.$imageInfo.style.display = 'none';
     }
   }
 }
